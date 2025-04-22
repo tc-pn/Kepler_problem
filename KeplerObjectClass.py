c073b627c2066092f000148ffaca5aa2d588e1f8
Thomas CZUBA <thomas.czuba1@gmail.com>
2025-04-22 19:21:14 +0200

    wip

--- KeplerObjectClass.py
+++ KeplerObjectClass.py
@@ -20,6 +20,8 @@ class KeplerObject():
         self.y_pos = y_pos # AU
         self.vx = vx # AU / yr
         self.vy = vy # AU / yr
+        self.trajectory = [(self.x_pos, self.y_pos, 
+                            self.vx, self.vy)]
 
     def update_vector(self,
                       new_x : float,
@@ -30,12 +32,14 @@ class KeplerObject():
         self.y_pos = new_y
         self.vx = new_vx
         self.vy = new_vy
+        self.trajectory.append((self.x_pos, self.y_pos, 
+                                self.vx, self.vy))
 
-    def accelerations(self,
-                      pos : float,
-                      mass_other : float,
-                      x_pos_other : float,
-                      y_pos_other : float) -> float:
+    def acceleration(self,
+                     pos : float,
+                     mass_other : float,
+                     x_pos_other : float,
+                     y_pos_other : float) -> float:
         d_x = np.abs(self.x_pos - x_pos_other)
         d_y = np.abs(self.y_pos - y_pos_other)
         out = - self.G * mass_other * pos / (d_x**2. + d_y**2.)**(3./2.)
--- SystemKeplerClass.py
+++ SystemKeplerClass.py
@@ -1,7 +1,151 @@
 """
 Module defining the KeplerSystem class.
 """
+import numpy as np
 from KeplerObjectClass import KeplerObject
 
 class KeplerSystem():
+    def __init__(self,
+                 m1 : float, m2 : float,
+                 x1_pos : float, x2_pos : float,
+                 y1_pos : float, y2_pos : float,
+                 vx1 : float, vx2 : float,
+                 vy1 : float, vy2 : float):
+        
+        self.G = 39.478 # AU**3 * year**-2. / solar mass
+        self.Object1 = KeplerObject(mass=m1,
+                                    x_pos=x1_pos,
+                                    y_pos=y1_pos,
+                                    vx=vx1,
+                                    vy=vy1)
+        self.Object2 = KeplerObject(mass=m2,
+                                    x_pos=x2_pos,
+                                    y_pos=y2_pos,
+                                    vx=vx2,
+                                    vy=vy2)
+        self.x1_ini = x1_pos #AU
+        self.x2_ini = x2_pos #AU
+        self.y1_ini = y1_pos #AU
+        self.y2_ini = y2_pos #AU
+        self.vx1_ini = vx1 #AU/yr
+        self.vx2_ini = vx2 #AU/yr
+        self.vy1_ini = vy1 #AU/yr
+        self.vy2_ini = vy2 #AU/yr
+
+    def solver_RK2(self,
+                nsteps : int=100,
+                delta_t : float=1./365.24) -> tuple:
+        
+        """
+        It is a standard RK2 procedure.
+
+        Parameters:
+        Object1 : KeplerObject
+            First object in the system
+        Object2 : KeplerObject
+            Second object in the system
+        nsteps: int
+            Number of time steps over which to solve the equations of motion
+
+        Returns:
+        Object1 : KeplerObject
+            First object of the system with updated positions and velocities
+        Object2 : KeplerObject
+            Second object of the system with updated positions and velocities
+        """
+        vector = [
+            self.Object1.x_pos, self.Object1.y_pos,
+            self.Object2.x_pos, self.Object2.y_pos,
+            self.Object1.vx, self.Object1.vy,
+            self.Object2.vx, self.Object2.vy
+        ]
+        vector = np.array(vector)
+
+        for i in range(nsteps):
+            vector_inter = self.calculate_step(Object1=self.Object1,
+                                        Object2=self.Object2,
+                                        vector=vector,
+                                        delta_t=delta_t/2.)
+            
+            vector_new = self.calculate_step(Object1=self.Object1,
+                                        Object2=self.Object2,
+                                        vector=vector_inter,
+                                        delta_t=delta_t)
+            
+            self.Object1.update_vector(new_x=vector_new[0],
+                                    new_y=vector_new[1],
+                                    new_vx=vector_new[4],
+                                    new_vy=vector_new[5])
+            
+            self.Object2.update_vector(new_x=vector_new[2],
+                                    new_y=vector_new[3],
+                                    new_vx=vector_new[6],
+                                    new_vy=vector_new[7])
+
+    def calculate_step(Object1 : KeplerObject,
+                       Object2 : KeplerObject,
+                       vector : np.ndarray,
+                       delta_t : float=1./365.24) -> np.ndarray:
+        """
+        Method allowing to update the position and velocities of the object as
+        The method is a standard RK2 procedure.
+
+        Parameters:
+        Object1 : KeplerObject
+            First object in the system
+        Object2 : KeplerObject
+            Second object in the system
+        vector : np.ndarray
+            Array containing the velocities and positions of both objects
+        delta_t : float
+            time step for time propagation
+
+        Returns:
+        vector_out : np.ndarray
+            Updated vector field of the Kepler Equations after timestep delta_t
+        """
+        accx_1 = Object1.acceleration(pos=Object1.x_pos,
+                                      mass_other=Object2.mass,
+                                      x_pos_other=Object2.x_pos,
+                                      y_pos_other=Object2.y_pos)
+        accy_1 = Object1.acceleration(pos=Object1.y_pos,
+                                      mass_other=Object2.mass,
+                                      x_pos_other=Object2.x_pos,
+                                      y_pos_other=Object2.y_pos)
+
+        accx_2 = - Object1.acceleration(pos=Object2.x_pos,
+                                      mass_other=Object1.mass,
+                                      x_pos_other=Object1.x_pos,
+                                      y_pos_other=Object1.y_pos)
+        accy_2 = - Object1.acceleration(pos=Object2.y_pos,
+                                      mass_other=Object1.mass,
+                                      x_pos_other=Object1.x_pos,
+                                      y_pos_other=Object1.y_pos)
+        
+
+        k = np.array([
+            Object1.vx, Object1.vy,
+            Object2.vx, Object2.vy,
+            accx_1, accy_1,
+            accx_2, accy_2
+        ])
+
+        vector_out = vector + delta_t * k
+                
+        return vector_out
+
+    def reinitialize_system(self):
+        self.Object1 = KeplerObject(mass=self.Object1.mass,
+                                    x_pos=self.x1_ini,
+                                    y_pos=self.y1_ini,
+                                    vx=self.vx1_ini,
+                                    vy=self.vy1_ini)
+        self.Object2 = KeplerObject(mass=self.Object2.mass,
+                                    x_pos=self.x2_ini,
+                                    y_pos=self.y2_ini,
+                                    vx=self.vx2_ini,
+                                    vy=self.vy2_ini)
+        
+
+    
     
