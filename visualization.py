e39fb479aa958483946c426cfee5073bea85610f
Thomas CZUBA <thomas.czuba1@gmail.com>
2025-04-22 21:47:50 +0200

    wip

--- visualization.py
+++ visualization.py
@@ -1,24 +1,29 @@
 """
 Visualization module.
 """
+import numpy as np
 import matplotlib.pyplot as plt 
 from matplotlib.animation import FuncAnimation
 from SystemKeplerClass import KeplerSystem
 
 def show_trajectory(System : KeplerSystem):
-    traj_1 = [coordinates[:1] for coordinates in System.Object1.trajectory]
-    traj_2 = [coordinates[:1] for coordinates in System.Object2.trajectory]
+    traj_1 = np.array([list(coordinates[:2]) for coordinates in System.Object1.trajectory])
+    traj_2 = np.array([list(coordinates[:2]) for coordinates in System.Object2.trajectory])
 
     fig, ax = plt.subplots(1,1)
-    line = ax.plot([], [], lw=2)
-    anim = FuncAnimation(fig, lambda x : animate(x, line=line, traj=traj_2), frames=len(traj_2), interval=len(traj_2))
-    plt.show(block=False)
+    line, = ax.plot([], [], lw=2)
+    #raise ValueError("")
+    anim = FuncAnimation(fig, lambda x : animate(x, line=line, traj=traj_2), 
+                         frames=len(traj_2),
+                         interval=1000)
+    #anim.save("test.gif")
+    plt.show()
     pass
 
 def animate(n : int, 
             line,
-            traj):
-    line.set_xdata(traj[:n][0])
-    line.set_ydata(traj[:n][1])
-    return line
+            traj : np.ndarray):
+    line.set_xdata(traj[:n, 0])
+    line.set_ydata(traj[:n, 1])
+    return line,
 
